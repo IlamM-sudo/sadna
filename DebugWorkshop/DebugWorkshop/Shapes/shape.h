8d284c348dfa2df2d1a1f644541385ed09531558
unknown <ilam1806mm@gmail.com>
2025-12-04 19:11:04 +0200

    fixed shapes

--- DebugWorkshop/DebugWorkshop/Shapes/shape.h
+++ DebugWorkshop/DebugWorkshop/Shapes/shape.h
@@ -9,7 +9,7 @@ private:
 
 public:
 	Shape();
-	virtual float get_area() const;
+	virtual float get_area(bool has_depth = false) const; //made the get_area functions the same so i could override it in triangle
 
 };
 

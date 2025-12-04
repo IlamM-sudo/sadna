87eefaa894fc28f353332cbd0d92abcbac831455
unknown <ilam1806mm@gmail.com>
2025-12-04 18:46:22 +0200

    finished fixing the LoopAgain bug

--- DebugWorkshop/DebugWorkshop/LoopAgain/LoopAgain.cpp
+++ DebugWorkshop/DebugWorkshop/LoopAgain/LoopAgain.cpp
@@ -9,7 +9,7 @@ int main()
 	std::cout << "what is the size of the series? ";
 	std::cin >> size;
 
-	while (size >= 0)
+	while (size > 0) //the loop was infinite because it is an unsigned int which means when doing -1 when size = 0 than it becomes the largest number that an unsigned int can have and doing it over and over infinitly
 	{
 		std::cout << t1 << ", ";
 
